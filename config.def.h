42c6949b11b9cd253f6870c0bec8b680827126bb
ultra <150538178+ultraKaiberry@users.noreply.github.com>
2026-03-06 02:17:02 +0530

    duplicated bind fix
    line 173 modkey+f binded to togglefullscreen
    line 233 modkey+f binded to fff
    changed line 233 to modkey+shift+f
    small change.

--- config.def.h
+++ config.def.h
@@ -230,7 +230,7 @@ static const Key keys[] = {
 /* application bindings */
 	{ MODKEY,			XK_m,          spawn,      {.v = (const char*[]){ "st", "-e", "termusic", NULL } } },
 	{ MODKEY,			XK_w,          spawn,      {.v = (const char*[]){ BROWSER, NULL } } },
-	{ MODKEY,			XK_f,          spawn,      {.v = (const char*[]){ "st", "-e", "fff", NULL } } },
+	{ MODKEY,ShiftMask,	XK_f,          spawn,      {.v = (const char*[]){ "st", "-e", "fff", NULL } } },
 	{ MODKEY,			XK_n,          spawn,      {.v = (const char*[]){ "st", "-e", "nvim", NULL } } },
 	{ MODKEY|ShiftMask,	XK_h,          spawn,      {.v = (const char*[]){ "st", "-e", "htop", NULL } } },
 	{ MODKEY,			XK_p,          spawn,      {.v = (const char*[]){ "darktable", NULL } } },
